652eee3f105d557d3231b347f086bbd5cd22cb71
Cesar Arroyo <cesardavid_09@hotmail.com>
2019-08-28 12:18:45 -0500

    "este es mi archivo nuevo"

---
+++ factorial.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+main(){
+	
+	int numero, fact=1,i;
+	
+	//printf("Ingrese el numero que desea saber su factorial: ");
+	//scanf("%d", &numero);
+	
+	//for(i=1; i<=numero; i++){
+	//	fact=fact*i;
+		
+		
+	//}
+	//printf("\nEl factorial es %d"  ,fact );
+	
+	printf("Ingrese el numero que desea saber su factorial: ");
+	scanf("%d", &numero);
+	
+	while(i<=numero){
+		fact=fact*i;
+		i++;
+	}
+	
+	printf("\nEl factorial es %d"  ,fact );
+	
+}
